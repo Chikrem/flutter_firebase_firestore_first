ab4d01a922acabd11f9f83781a172fdd2dc5c045
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-01-30 17:18:13 -0300

    Lendo de Subcoleções do Firestore

--- lib/firestore/presentation/home_screen.dart
+++ lib/firestore/presentation/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_firestore_first/firestore_produtos/presentation/produto_screen.dart';
 import 'package:uuid/uuid.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // Início Curso 2 Sub-listas
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase_firestore_first/firestore/presentation/home_screen.dart';
