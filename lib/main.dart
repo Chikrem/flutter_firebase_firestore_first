114b175366be031494f379bfd931ccd6065e9d58
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-03 17:36:51 -0300

    Final Curso 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase_firestore_first/_core/mycolors.dart';
 import 'package:flutter_firebase_firestore_first/firestore/presentation/home_screen.dart';
 import 'firebase_options.dart';
 
@@ -18,11 +19,30 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lista de Compras',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.brown),
+        appBarTheme: const AppBarTheme(
+          color: MyColors.brown,
+          toolbarHeight: 72,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+          foregroundColor: Colors.white,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: MyColors.green,
+        ),
+        listTileTheme: const ListTileThemeData(
+          iconColor: MyColors.blue,
+        ),
         useMaterial3: true,
       ),
       home: const HomeScreen(),
