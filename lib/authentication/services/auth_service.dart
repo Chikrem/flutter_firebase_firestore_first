afea9d7097917f545b297fd14b6e35993ab36585
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-04 16:58:03 -0300

    Ajustando tratamento de excessão

--- lib/authentication/services/auth_service.dart
+++ lib/authentication/services/auth_service.dart
@@ -3,12 +3,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  entrarUsuario({required String email, required String senha}){
+  entrarUsuario({required String email, required String senha}) {
     print("Método de Entrada");
   }
 
-  cadastrarUsuario({required String email, required String senha, required String nome}){
-    print("Método de Cadastro");
+  cadastrarUsuario(
+      {required String email,
+      required String senha,
+      required String nome}) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: senha);
+      await userCredential.user!.updateDisplayName(nome);
+      print("Método de Cadastro");
+    } on FirebaseAuthException catch (e) {
+      switch(e.code){
+        case "email-already-in-use":
+          return "O e-mail já está cadastrado.";
+      }
+      return e.code;
+    }
   }
-
 }
